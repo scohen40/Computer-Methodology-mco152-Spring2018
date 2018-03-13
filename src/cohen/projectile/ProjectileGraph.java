e050e34c2b52cfe20d0ef4f15ed1252228a1042c
Sarah Cohen <cohen.sarah591@gmail.com>
2018-03-13 18:55:26 -0400

    projGraph update

--- src/cohen/projectile/ProjectileGraph.java
+++ src/cohen/projectile/ProjectileGraph.java
@@ -1,60 +1,98 @@
 package cohen.projectile;
 
+import javax.imageio.ImageIO;
 import javax.swing.JComponent;
+
+import com.sun.prism.Image;
 
 import java.awt.Canvas;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.List;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.*;
 
 public class ProjectileGraph extends JComponent {
+	//Graphics graph;
+	private BufferedImage projectileImage;
+	private Projectile projectile1 = new Projectile(45, 150);
+	private Projectile projectile2 = new Projectile(40, 160);
+	private Projectile projectile3 = new Projectile(75, 200);
+
 	
 	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
+		 try {
+				projectileImage = ImageIO.read(new File("src/cohen/projectile/projectile-icon.png"));
+		  } catch (IOException e) {
+			System.out.println("projectile image for search button could not be loaded. contact system admin.");
+		  }		
+		super.paintComponent(g);	
 		
-		Projectile projectile = new Projectile(80, 120);
-		g.translate(0, this.getHeight());
+		g.translate(0, this.getHeight());	
 		
-		ArrayList<Integer> xPoints = new ArrayList();
-		ArrayList<Integer> yPoints = new ArrayList();
-		int nPoints = 0;
 		g.setColor(Color.RED);	
-		for(int i = 0; i < 20; i++) {
-			int x = (int)projectile.getX(i);
-			int y = (int)projectile.getY(i);		
-				
-			
-			g.fillOval(x, -y, 5, 5);
-			g.drawString("("+x+", "+y+")", x, -y);	
-			
-			xPoints.add(x);
-			yPoints.add(y);
-			nPoints++;
-		}	
-		nPoints++;
-		for(int i = 0; i < nPoints; i++) {
-			if(xPoints.get(i+1) != null && yPoints.get(i+1) != null)
-			{
-				g.drawLine(xPoints.get(i), -yPoints.get(i), xPoints.get(i+1), -yPoints.get(i+1));
-			}
-			else
-			{
-				break;
-			}
-			
-		}
+		drawProjectilePath20Sec(g, projectile1);
+		g.setColor(Color.BLUE);
+		drawProjectilePath20Sec(g, projectile2);
+		g.setColor(Color.GREEN);
+		drawProjectilePath20Sec(g, projectile3);
+		
 		
 	}
+
+	public void drawProjectilePath20Sec(Graphics g, Projectile proj) {
+		ArrayList<Integer> xPoints = new ArrayList();
+		ArrayList<Integer> yPoints = new ArrayList();
+		//plot the coordinates and fill them
+		for(int i = 0; i < 20; i++) {
+			int x = (int)proj.getX(i);
+			int y = (int)proj.getY(i);	
+			g.fillOval(x, -y, 5, 5);
+			g.drawString("("+x+", "+y+")", x, -y);			
+			xPoints.add(x);
+			yPoints.add(y);
+		}	
+		//connect the dots and add cute projectile icon for our pain
+		for(int i = 0; i < xPoints.size()-1; i++) {
+			g.drawLine(xPoints.get(i), -yPoints.get(i), xPoints.get(i+1), -yPoints.get(i+1));	
+			
+		}
+		//
+		
+		g.drawImage(projectileImage, 45, -45, null);
+
+	}
+	
+//	//put in grid lines because our professor hates us
+//	public void addGridLines()
+//	{
+//		
+//	}
+	
+	
+	
+	
 	
 }
 
 
-//hw 1: connect all the dots the lines to connect them  -----????
+
+
+
+
+
+
+
+
+
+
+
+//hw 1: connect all the dots the lines to connect them  -----done
 //hw 2: draw the coordinates(string) ----done
 //hw 3: colors, fill the dots in ----done
 //hw 4: put in graph lines  ----????
-//hw 5: projectile image		----do 2nd to last
-//hw 6: multiple projectiles, 3 with different color lines, different numbers -----do at the end (repeat the loops with diff colors)
-
+//hw 5: projectile image		----????
+//hw 6: multiple projectiles, 3 with different color lines, different numbers -----done
 
